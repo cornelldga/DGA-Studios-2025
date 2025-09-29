db0d1554e3c57a021da9c23bf0d8bc4832e10143
Angel <thesparkingofhope@gmail.com>
2025-09-29 17:51:50 -0400

    Add Health Bar

--- Assets/Scripts/BossManager.cs
+++ Assets/Scripts/BossManager.cs
@@ -2,25 +2,30 @@ using UnityEngine;
 
 public class BossManager : MonoBehaviour
 {
-    private int health;
+    private static int health;
     [SerializeField] private int startingHealth;
-    [SerializeField] private Transform healthBar;
-    [SerializeField] private Transform backdrop;
+    [SerializeField] private GameObject healthBar;
+    [SerializeField] private GameObject healthBackDrop;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         health = startingHealth;
-        healthBar.localScale.Set(health,1,1);
-        backdrop.localScale.Set(health, 1, 1);
+        healthBar.SetActive(true);
+        healthBackDrop.SetActive(true);
+        healthBackDrop.transform.localScale = new Vector3(startingHealth,1,1);
+        healthBar.transform.localScale = new Vector3(startingHealth, 1, 1);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        healthBar.transform.localScale = new Vector3(health, 1, 1);
         if (health == 0)
         {
             Time.timeScale = 0;
         }
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
