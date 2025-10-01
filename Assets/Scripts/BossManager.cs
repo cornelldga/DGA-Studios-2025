353683a08e20ba91c68a2cd26973fd627f743715
Angel <thesparkingofhope@gmail.com>
2025-10-01 16:32:06 -0400

    Add Basic Movement to Boss Object

--- Assets/Scripts/BossManager.cs
+++ Assets/Scripts/BossManager.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public class BossManager : MonoBehaviour
@@ -6,6 +7,16 @@ public class BossManager : MonoBehaviour
     [SerializeField] private int startingHealth;
     [SerializeField] private GameObject healthBar;
     [SerializeField] private GameObject healthBackDrop;
+    private enum MovementType
+    {
+        Standing,MoveRight,MoveLeft,MoveUp,MoveDown,Circle,Line
+    } 
+    private MovementType movementType;
+    private float timer;
+    //Temp variables
+    private int moves;
+    private int right;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,17 +25,40 @@ public class BossManager : MonoBehaviour
         healthBackDrop.SetActive(true);
         healthBackDrop.transform.localScale = new Vector3(startingHealth,1,1);
         healthBar.transform.localScale = new Vector3(startingHealth, 1, 1);
+        movementType=MovementType.Line;
+        timer = 0;
+        moves = 0;
+        right = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         healthBar.transform.localScale = new Vector3(health, 1, 1);
+        
         if (health == 0)
         {
             Time.timeScale = 0;
         }
-
+        
+        //Imagined implementation: Select a location (Add Parameter?) and boss will pathfind, teleport, or jump to that location
+        if (movementType != MovementType.Standing)
+        {
+            if (movementType == MovementType.Line)
+            {
+                if (timer > .01f )
+               { transform.position = transform.position + new Vector3(.05f*right, 0, 0);
+                    timer = 0;
+                    moves++;
+                }
+                if (moves == 300/5)
+                {
+                    right = right *-1;
+                    moves = 0;
+                }
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
