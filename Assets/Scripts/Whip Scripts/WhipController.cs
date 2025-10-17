708bca0229be086bf4f8acab7196e694044c1695
ma2384 <mikaelandreasyan@gmail.com>
2025-10-17 01:14:58 -0400

    Improved Warping
    - The warping has been made better by adjusting the result of the angle calculation to not be as extreme

--- Assets/Scripts/Whip Scripts/WhipController.cs
+++ Assets/Scripts/Whip Scripts/WhipController.cs
@@ -12,6 +12,11 @@ public class WhipController : MonoBehaviour
     private bool whipping;
     private float timer;
 
+    //a magical number that I use to divide the offset of an angle from the angle it should move towards
+    //this helps me make that micromovement I need to move the whip in a way that is less warped.
+    //the bigger the number, the less we will adjust
+    private float MAGIC_ADJUSTMENT_RATIO = 3f;
+
     private void OnEnable()
     {
         playerControls = new PlayerInputActions();
@@ -37,6 +42,46 @@ public class WhipController : MonoBehaviour
         }
     }
 
+    public float AngleAdjustment(float originalAngle)
+    {
+        float finalAngle = originalAngle;
+        float sign = originalAngle / Mathf.Abs(originalAngle);
+        float adjustAmountAbs = 0;
+        if (originalAngle >= -45 && originalAngle <= 45)
+        {
+            //adjust against 0
+            adjustAmountAbs = Mathf.Abs(originalAngle);
+        } else if (originalAngle <= -45  && originalAngle >= -135)
+        {
+            adjustAmountAbs = Mathf.Abs(90 - Mathf.Abs(originalAngle));
+            //adjust against -90
+        } else if (originalAngle <= 135 && originalAngle >= 45)
+        {
+            adjustAmountAbs = Mathf.Abs(90 - Mathf.Abs(originalAngle));
+            //adjust against 90
+            
+        } else if (originalAngle >= 135 || originalAngle <= -135)
+        {
+            //adjust against 180
+            adjustAmountAbs = 180 - Mathf.Abs(originalAngle);
+        }
+
+
+
+
+        if (Mathf.Abs(originalAngle) < 90)
+        {
+            //adjust towards 0
+            finalAngle = originalAngle - (sign * adjustAmountAbs / MAGIC_ADJUSTMENT_RATIO);
+        }
+        else if (Mathf.Abs(originalAngle) > 90)
+        {
+            //adjust towards 180
+            finalAngle = originalAngle + (sign * adjustAmountAbs / MAGIC_ADJUSTMENT_RATIO);
+        } 
+        return finalAngle;
+    }
+
     public void OnWhip()
     {
         whipping = true;
@@ -48,6 +93,8 @@ public class WhipController : MonoBehaviour
         mousePosition.z = 0f;
         Vector3 direction = mousePosition - whipPivot.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Debug.Log(angle);
+        angle = AngleAdjustment(angle);
         whipPivot.transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 
