641bb1fe986e7921930c4bb30432a3f7b1089b22
Annabelle <annabelle.ford.314@gmail.com>
2025-10-27 18:06:11 -0400

    boss dialogue boxes work and deleted scene changer

--- Assets/Dialogue/DialogueTrigger.cs
+++ Assets/Dialogue/DialogueTrigger.cs
@@ -11,6 +11,7 @@ public class DialogueTrigger : MonoBehaviour, IInteractable
     [SerializeField] Sprite neutralSprite;
     [SerializeField] Sprite happySprite;
     [SerializeField] Sprite sadSprite;
+    [SerializeField] string sceneName = "";
     private int progressionInt;
 
     Dictionary<DialogueEmotion, Sprite> emotionDictionary = new Dictionary<DialogueEmotion, Sprite>();
@@ -38,7 +39,7 @@ public class DialogueTrigger : MonoBehaviour, IInteractable
         if (!DialogueManager.Instance.OngoingDialogue())
         {
             DialogueManager.Instance.StartDialogue(jsonTextFile, progressionInt,
-            dialogueBoxSprite, emotionDictionary);
+            dialogueBoxSprite, emotionDictionary, sceneName);
         }
             
     }
