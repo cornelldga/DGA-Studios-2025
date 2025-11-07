516c725dbc10b1abcb124da36d3ec132e1ae447c
Annabelle <annabelle.ford.314@gmail.com>
2025-11-07 17:19:44 -0500

    small changes

---
+++ Assets/Scripts/LoadoutManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadoutManager : MonoBehaviour
+{
+    [SerializeField] private Button firstMixer;
+    [SerializeField] private Button secondMixer;
+    [SerializeField] private Button thirdMixer;
+    [SerializeField] private Button fourthMixer;
+    [SerializeField] private Button firstBase;
+    [SerializeField] private Button secondBase;
+    [SerializeField] private Button thirdBase;
+    [SerializeField] private Button fourthBase;
+    [SerializeField] private Button beginFight;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
