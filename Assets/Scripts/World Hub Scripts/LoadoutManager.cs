fbd86bf881fb848ecf43f4c6bd16bbaf1c58f51d
Annabelle <annabelle.ford.314@gmail.com>
2025-11-10 18:29:22 -0500

    more ui logic

--- Assets/Scripts/World Hub Scripts/LoadoutManager.cs
+++ Assets/Scripts/World Hub Scripts/LoadoutManager.cs
@@ -16,11 +16,11 @@ public class LoadoutManager : MonoBehaviour
     [SerializeField] private Button secondBase;
     [SerializeField] private Button thirdBase;
     [SerializeField] private Button fourthBase;
-    [SerializeField] private Image baseSlotOne;
-    [SerializeField] private Image baseSlotTwo;
-    [SerializeField] private Image mixerSlotOne;
-    [SerializeField] private Image mixerSlotTwo;
-    private Image currentSlot;
+    [SerializeField] private Sprite baseSlotOne;
+    [SerializeField] private Sprite baseSlotTwo;
+    [SerializeField] private Sprite mixerSlotOne;
+    [SerializeField] private Sprite mixerSlotTwo;
+    private Sprite currentSlot;
 
     public void SelectMixer(Button buttonSelected)
     {
@@ -29,25 +29,25 @@ public class LoadoutManager : MonoBehaviour
         {
             mixer = MixerType.Cider;
             firstMixer.gameObject.SetActive(false);
-            baseSlotOne.sprite = firstMixer.GetComponent<Sprite>();
+            currentSlot = firstMixer.GetComponent<Sprite>();
         }
         else if (buttonSelected == secondMixer)
         {
             mixer = MixerType.Ginger;
             secondMixer.gameObject.SetActive(false);
-            baseSlotOne.sprite = secondMixer.GetComponent<Sprite>();
+            currentSlot = secondMixer.GetComponent<Sprite>();
         }
         else if (buttonSelected == thirdMixer)
         {
             mixer = MixerType.Lime;
             thirdMixer.gameObject.SetActive(false);
-            baseSlotOne.sprite = thirdMixer.GetComponent<Sprite>();
+            currentSlot = thirdMixer.GetComponent<Sprite>();
         }
         else
         {
             mixer = MixerType.Pimiento;
             fourthMixer.gameObject.SetActive(false);
-            baseSlotOne.sprite = fourthMixer.GetComponent<Sprite>();
+            currentSlot = fourthMixer.GetComponent<Sprite>();
         }
         // change it from just zero?
         GameManager.Instance.player.SwapMixerSlot(0, mixer);
@@ -60,34 +60,44 @@ public class LoadoutManager : MonoBehaviour
         {
             baseType = BaseType.Beer;
             firstBase.gameObject.SetActive(false);
+            currentSlot = firstBase.GetComponent<Sprite>();
         }
         else if (buttonSelected == secondBase)
         {
             baseType = BaseType.Gin;
             secondBase.gameObject.SetActive(false);
+            currentSlot = secondBase.GetComponent<Sprite>();
         }
         else if (buttonSelected == thirdBase)
         {
             baseType = BaseType.Whiskey;
             thirdBase.gameObject.SetActive(false);
+            currentSlot = thirdBase.GetComponent<Sprite>();
         }
         else
         {
             baseType = BaseType.Wine;
             fourthBase.gameObject.SetActive(false);
+            currentSlot = fourthBase.GetComponent<Sprite>();
         }
         // change it from just zero?
         GameManager.Instance.player.SwapBaseSlot(0, baseType);
     }
 
-    public void Slot(Image slot)
+    public void Slot(Sprite slot)
     {
         if (slot==baseSlotOne)
         {
             currentSlot = baseSlotOne;
         } else if (slot==baseSlotTwo)
         {
-            
+            currentSlot = baseSlotTwo;
+        } else if (slot==mixerSlotOne)
+        {
+            currentSlot = mixerSlotOne;
+        } else
+        {
+            currentSlot = mixerSlotTwo;
         }
     }
 
