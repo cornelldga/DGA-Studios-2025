9e9d00986eae80474b5b8acaf0731c5e55233d2f
julyc25 <yljcai999@gmail.com>
2025-11-12 17:57:35 -0500

    Added Dynamite Documentation

--- Assets/Scripts/Dynamite.cs
+++ Assets/Scripts/Dynamite.cs
@@ -2,22 +2,26 @@ using System;
 using UnityEngine;
 using UnityEngine.Analytics;
 using Unity.Cinemachine;
-//The dynamite is a non-moving aoe attack. Although not a "real projectile," it has overlapping features
+/// <summary>
+/// Dynamites that the drill boss spawns
+/// </summary>
 public class Dynamite : MonoBehaviour
 {
+    [Tooltip("True when the dynamite is exploding")]
+    private bool isExploding;
+
     public float max_radius;
     private float curr_radius;
     private float last_time;
     public float time_between;
     public float num_increases;
     public float damage;
-    //damage only dealt when isExploding = true;
-    private bool isExploding;
     [SerializeField] Transform transform_object;
     [SerializeField] private CinemachineImpulseSource impulseSource;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    /// <summary>
+    /// Dynamites that the drill boss spawns
+    /// </summary>
     void Start()
     {
         isExploding = false;
@@ -25,15 +29,17 @@ public class Dynamite : MonoBehaviour
         time_between = 0.001f;
         curr_radius = transform_object.localScale.x / 2;
         damage = 1;
-        Explode();
     }
 
-    // Update is called once per frame
+
+    /// <summary>
+    /// On update, if the dynamite is exploding, the affect area increases
+    /// </summary>
     void Update()
     {
         if (isExploding)
         {
-            if (curr_radius >= max_radius){isExploding = false;}
+            if (curr_radius >= max_radius) { isExploding = false; }
             else
             {
                 float curr_time = Time.time;
@@ -49,6 +55,9 @@ public class Dynamite : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Sets isExplosion() to true and shakes screen.
+    /// </summary>
     void Explode()
     {
         isExploding = true;
@@ -56,13 +65,16 @@ public class Dynamite : MonoBehaviour
         if (impulseSource != null)
             impulseSource.GenerateImpulse(0.5f);
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         OnAttackHit(collision);
     }
 
-     public virtual void OnAttackHit(Collider2D collision)
+    /// <summary>
+    /// If contacts player during explosion, then deals damage.
+    /// </summary>
+    public virtual void OnAttackHit(Collider2D collision)
     {
         if (isExploding && collision.CompareTag("Player"))
             collision.gameObject.GetComponent<IDamageable>().TakeDamage(damage);
