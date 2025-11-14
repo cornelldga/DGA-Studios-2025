5713f2718e4e11c290257443d0b4d87a3d68a9f0
Andrew Hu <94906754+Tofucs@users.noreply.github.com>
2025-11-14 13:13:55 -0500

    pr changes

--- Assets/Scripts/DrillHole.cs
+++ Assets/Scripts/DrillHole.cs
@@ -4,22 +4,25 @@ using System.Collections;
 /// <summary>
 /// Represents a single hole in the ground object caused from Drill boss entering and exiting states
 /// </summary>
-public class DrillHole : Obstacle, IDamageable
+public class DrillHole : MonoBehaviour, IDamageable
 {
     // if this hole is initialized exiting underground
-    private bool emersionHole;
+    private Rigidbody2D rb;
+    [SerializeField] private bool emersionHole;
 
     [Header("Rock Splash Settings")]
+    // change to rock debris when possible
+    [SerializeField] private Base debrisPrefab;
     [SerializeField] private float spawnOffset = 0.2f;
 
-    // pass this onto the rock debris objects, its the radius of the splash attack
-    [SerializeField] private float radius = 3f;
+    // the radius of the splash attack, to be passed onto the rock debris objects
+    [SerializeField] private float maxRadius = 3f;
     [SerializeField] private int debrisCount = 10;
     [SerializeField] private float debrisSpeed = 5f;
 
-    void Start()
+    public void Start()
     {
-        base.Start();
+        rb = GetComponent<Rigidbody2D>();
         // TODO: Should the emersion rock debris damage be called here?
         if (emersionHole)
         {
@@ -27,26 +30,43 @@ public class DrillHole : Obstacle, IDamageable
         }
     }
 
-    void Update()
+    public void Update()
     {
-        base.Update();
+        
     }
 
     public void TakeDamage(float damage)
     {
-        Deactivate();
+        Destroy(gameObject);
+    }
+
+    public Vector3 GetPosition()
+    {
+        return rb.position;
     }
     
+    /// <summary>
+    /// Initializes debris projectiles evenly in a ring around the hole that damage player
+    /// Should use maxRadius to specify how far projectiles will travel
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator BlastDebris()
     {
         for (int i = 0; i < debrisCount; i++)
         {
             float angle = i * (360f / debrisCount) * Mathf.Deg2Rad;
-            Vector3 offset = new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)) * spawnOffset;
+            Vector3 offset = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0f) * spawnOffset;
             Vector3 spawnPosition = transform.position + offset;
             Vector3 velocity = offset.normalized * debrisSpeed;
+            Quaternion direction = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
 
             // TODO: instantiate the rock Debris objects, after we do PRs for the other objects
+            Base debris = Instantiate(debrisPrefab, spawnPosition, direction);
+            Rigidbody2D debrisRb = debris.GetComponent<Rigidbody2D>();
+            if (debrisRb != null)
+            {
+                debrisRb.linearVelocity = velocity;
+            }
         }
         yield return null;
     }
