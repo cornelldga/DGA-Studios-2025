2cea965aa13a11d6fc7d5cc3506f2c6072f687c7
Andrew Hu <94906754+Tofucs@users.noreply.github.com>
2025-11-21 14:11:13 -0500

    Drill guy entering/exiting states
    Added basic walking state (just waits), and then a enter state that spawns a hole and an exit state that also spawns a hole. Also, added a drill path class that is basically a bezier curve to make underground movement interesting, an created test scene for the digging push_back and state behavior loop.

--- Assets/Scripts/Boss Scripts/Drill_Guy.cs
+++ Assets/Scripts/Boss Scripts/Drill_Guy.cs
@@ -3,6 +3,7 @@ using Unity.Cinemachine;
 using System;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEngine.Splines;
 
 public class Drill_Guy : Boss
 {
@@ -17,8 +18,30 @@ public class Drill_Guy : Boss
     //Time until we should change states.
     private float stateTimer;
     private Rigidbody2D rb;
+    private bool isUnderground;
     private CinemachineImpulseSource impulseSource;
     private List<GameObject> holes; //holes
+
+    [Header("Hole Settings")]
+
+    [Tooltip("The drill hole prefab object that is created by Drill Guy's transition from underground to aboveground")]
+    [SerializeField] private GameObject exitHolePrefab;
+
+    [Tooltip("The drill hole prefab object that is created by Drill Guy's transition from aboveground to underground")]
+    [SerializeField] private GameObject enterHolePrefab;
+
+    [Header("Dig Settings")]
+
+    [Tooltip("Specifies the max push strength. Increase this to cause more dispalcement from the drill dig path.")]
+    [SerializeField] private float pushForce = 30f;
+    // the path we take in digging
+    private DrillPath currentPath;
+    // how far along we are along this path
+    private float t;
+    // how fast we should complete this dig (DO NOT GO OVER 2 ITS OP!)
+    private float speedModifier = 0.8f;
+    private float pushRadius = 1f;
+    private Collider2D digCollider;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,8 +50,9 @@ public class Drill_Guy : Boss
         base.Start();
         rb = GetComponent<Rigidbody2D>();
         impulseSource = GetComponent<CinemachineImpulseSource>();
-        currentState = State.Targeting;
+        currentState = State.Walking;
         stateTimer = targetingTime;
+        isUnderground = false;
     }
 
     /// <summary>
@@ -37,7 +61,7 @@ public class Drill_Guy : Boss
     public override void Update()
     {
         base.Update();
-
+        Debug.Log(currentState);
         stateTimer -= Time.deltaTime;
 
         switch (currentState)
@@ -66,9 +90,16 @@ public class Drill_Guy : Boss
         }
     }
 
+    /// <summary>
+    /// What the boss does when walking around
+    /// Planning for attacking the player is done in Targeting
+    /// </summary>
     private void UpdateWalking()
     {
-        throw new NotImplementedException();
+        if (stateTimer <= 0)
+        {
+            currentState = State.Entering;
+        }
     }
 
     private void UpdateTargeting()
@@ -78,7 +109,10 @@ public class Drill_Guy : Boss
 
     private void UpdateUG_Chase()
     {
-        throw new NotImplementedException();
+        if (t >= 1f)
+        {
+            currentState = State.Exiting;
+        }
     }
 
     private void UpdateUG_Random()
@@ -86,16 +120,129 @@ public class Drill_Guy : Boss
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Updates the Drill boss during Entering state.
+    /// </summary>
     private void UpdateEntering()
     {
-        throw new NotImplementedException();
+        isUnderground = true;
+        Instantiate(enterHolePrefab, transform.position, Quaternion.identity);
+
+        CreateChasePathToPlayer();
+        StartCoroutine(DigPath());
+        
+        currentState = State.Underground_Chase;
     }
 
+    /// <summary>
+    /// Updates the Drill boss during Exiting state.
+    /// </summary>
     private void UpdateExiting()
     {
-        throw new NotImplementedException();
+        isUnderground = false;
+        Instantiate(exitHolePrefab, transform.position, Quaternion.identity);
+
+        currentState = State.Walking;
+        stateTimer = 1f;
     }
 
+    /// <summary>
+    /// Generates a cubic bezier path from current position to a little bit player position
+    /// Prob should be called in target?
+    /// p1 and p2 are random as of now.
+    /// </summary>
+    private void CreateChasePathToPlayer()
+    {
+        Vector2 p0 = transform.position; 
+        Vector2 p3 = GameManager.Instance.player.transform.position; 
+        
+        Vector2 directionToPlayer = (p3 - p0).normalized;
+        p3 += directionToPlayer * 1.5f;
+        float distanceToPlayer = Vector2.Distance(p0, p3);
+        
+        Vector2 p1 = GenerateControlPoint(p0, p3, directionToPlayer, distanceToPlayer, 0.15f);
+        
+        Vector2 p2 = GenerateControlPoint(p0, p3, directionToPlayer, distanceToPlayer, 0.85f);
+        
+        currentPath = new DrillPath(p0, p1, p2, p3);
+        t = 0f;
+    }
+
+    /// <summary>
+    /// Generates a random control point between p0 and p3, with restriction that it 
+    /// its projection to the line p0 p3 is stricly in between them
+    /// Moidfy this (or extend with multiple splines) to make drill path more interesting
+    /// </summary>
+    /// <param name="p0"></param>
+    /// <param name="p3"></param>
+    /// <param name="forward"></param>
+    /// <param name="distance"></param>
+    /// <param name="tAlong"></param>
+    /// <returns></returns>
+    private Vector2 GenerateControlPoint(Vector2 p0, Vector2 p3, Vector2 forward, float distance, float tAlong)
+    {
+        Vector2 basePoint = p0 + forward * distance * tAlong;
+        
+        Vector2 perpendicular = new Vector2(-forward.y, forward.x);
+        float randomOffset = UnityEngine.Random.Range(-distance, distance);
+        
+        Vector2 controlPoint = basePoint + perpendicular * randomOffset;
+        
+        Vector2 toControl = controlPoint - p0;
+        Vector2 toEnd = p3 - p0;
+        
+        if (Vector2.Dot(toControl, toEnd) < 0)
+        {
+            controlPoint = basePoint;
+        }
+        
+        return controlPoint;
+    }
+
+    /// <summary>
+    /// Basically the action that is completed during UG_Chase or UG_Random
+    /// When t hits 1, the path is complete.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator DigPath()
+    {
+
+        // dig movement
+        while (t < 1)
+        {
+            t += Time.deltaTime * speedModifier;
+            Vector2 newPos = currentPath.GetPositionForT2D(t);
+            transform.position = newPos;
+            yield return new WaitForEndOfFrame();
+            Debug.Log($"t={t}, position={newPos}");
+        }
+        
+        t = 0f;
+    }
+
+    /// <summary>
+    /// Called when another object enters the trigger collider.
+    /// Pushes back the player when the drill guy is digging underneath them.
+    /// </summary>
+    /// <param name="other"></param>
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody2D playerRb = other.GetComponent<Rigidbody2D>();
+            Vector2 dir = (other.transform.position - transform.position).normalized;
+            float t = (other.transform.position - transform.position).magnitude / pushRadius;
+            float scale = Mathf.Clamp(t, 0.4f, 1f);
+            scale = 1f / scale;
+            if (playerRb != null)
+            {
+                playerRb.AddForce(dir * pushForce * scale, ForceMode2D.Force);
+            } else
+            {
+                Debug.Log("Drill push back trigger can not find PlayerRb");
+            }
+        }
+    }
 
     /// <summary>
     /// Dynamite throwing.
@@ -106,5 +253,10 @@ public class Drill_Guy : Boss
             Vector2 target = holes[i].transform.position;
             //Throw dynamite in that direction.
         }
+    }
+
+    public override void SetPhase()
+    {
+        
     }
 }
