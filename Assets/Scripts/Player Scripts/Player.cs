a21719e54628cba9421dd4bc2a3e92c0dd3be6b6
Mikael Andreasyan <mikaelandreasyan@gmail.com>
2025-12-01 16:08:53 -0500

    Finalized animation
    - Also, no more magic ratio!

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -58,11 +58,6 @@ public class Player : MonoBehaviour, IDamageable
     [SerializeField] Image mixerEquippedImage;
     [SerializeField] Image mixerBackupImage;
 
-    //a magical number that I use to divide the offset of an angle from the angle it should move towards
-    //this helps me make that micromovement I need to move the whip in a way that is less warped.
-    //the bigger the number, the less we will adjust
-    private float MAGIC_ADJUSTMENT_RATIO = 5f;
-
     Animator animationControl;
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
@@ -268,63 +263,14 @@ public class Player : MonoBehaviour, IDamageable
     }
 
     /// <summary>
-    /// Returns an adjusted angle that makes the whip's final location more accurate to where the user clicked
-    /// </summary>
-    public float AngleAdjustment(float originalAngle)
-    {
-        float finalAngle = originalAngle;
-        float sign = originalAngle / Mathf.Abs(originalAngle);
-        float adjustAmountAbs = 0;
-        if (originalAngle >= -45 && originalAngle <= 45)
-        {
-            //adjust against 0
-            adjustAmountAbs = Mathf.Abs(originalAngle);
-        }
-        else if (originalAngle <= -45 && originalAngle >= -135)
-        {
-            adjustAmountAbs = Mathf.Abs(90 - Mathf.Abs(originalAngle));
-            //adjust against -90
-        }
-        else if (originalAngle <= 135 && originalAngle >= 45)
-        {
-            adjustAmountAbs = Mathf.Abs(90 - Mathf.Abs(originalAngle));
-            //adjust against 90
-
-        }
-        else if (originalAngle >= 135 || originalAngle <= -135)
-        {
-            //adjust against 180
-            adjustAmountAbs = 180 - Mathf.Abs(originalAngle);
-        }
-
-        if (Mathf.Abs(originalAngle) < 90)
-        {
-            //adjust towards 0
-            finalAngle = originalAngle - (sign * adjustAmountAbs / MAGIC_ADJUSTMENT_RATIO);
-        }
-        else if (Mathf.Abs(originalAngle) > 90)
-        {
-            //adjust towards 180
-            finalAngle = originalAngle + (sign * adjustAmountAbs / MAGIC_ADJUSTMENT_RATIO);
-        }
-        return finalAngle;
-    }
-    /// <summary>
     /// Rotates the whip towards the mouse and 
     /// </summary>
     public void OnWhip()
     {
         whipping = true;
         whip.gameObject.GetComponent<EdgeCollider2D>().enabled = true;
-        //find angle between player and mouse
-        //whipObject.transform.rotation = Quaternion.identity;
-        //Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        //mousePosition.z = 0f;
-        //Vector3 direction = mousePosition - whipPivot.position;
         whipPivotAnimator.Play("Whip Rotate", 0, 0f);
-        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        //angle = AngleAdjustment(angle);
-        //whipPivot.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        whipAnimator.Play("Whip", 0, 0f);
         StartCoroutine(nameof(WhipTime));
     }
 
