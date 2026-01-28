858a6ea35b60758796bea5fdf8aade11068e57bc
julyc25 <yljcai999@gmail.com>
2026-01-28 17:57:12 -0500

    Added Ash Prefab + Script

---
+++ Assets/Scripts/Boss Scripts/Ash.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Ash : Boss
+{
+    public override void SetPhase()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
