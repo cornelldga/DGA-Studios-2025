0ba50cc0acf3021129aeacf5d0a2a4dce1d9f40a
Samantha Cruz <samantha.jaycruz@gmail.com>
2026-02-03 01:48:55 -0500

    Clean up GameManager.cs

--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -8,23 +8,20 @@ using System.Collections;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    [SerializeField] LoadoutManager loadoutManager;
-    [SerializeField] Animator animator;
+    
+    [Header("References")]
+    [Tooltip("Reference to the LoadoutManager.")]
+    [SerializeField] private LoadoutManager loadoutManager;
+    
+    [Tooltip("Animator for scene transitions")]
+    [SerializeField] private Animator animator;
+    
     [HideInInspector] public Player player;
 
-
-    /// <summary>
-    /// Sets the loadout manager to active.
-    /// </summary>
-    public void ToggleLoadoutManager(bool isOpen)
-    {
-        FreezePlayer(isOpen);
-        loadoutManager.gameObject.SetActive(isOpen);
-    }
-
-
     [Header("World Settings")]
+    [Tooltip("The background music for the current scene")]
     [SerializeField] private MusicType currentSong;
+    
     public MusicType CurrentSong => currentSong;
 
     private void Awake()
@@ -39,9 +36,23 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        
     }
 
+    /// <summary>
+    /// Sets the loadout manager to active.
+    /// </summary>
+    /// <param name="isOpen">If the loadout manager should be open</param>
+    public void ToggleLoadoutManager(bool isOpen)
+    {
+        FreezePlayer(isOpen);
+        loadoutManager.gameObject.SetActive(isOpen);
+    }
+
+    /// <summary>
+    /// Gets the currently equipped bases and mixers from the loadout.
+    /// </summary>
+    /// <param name="equippedBases">Array to store equipped bases</param>
+    /// <param name="equippedMixers">Array to store equipped mixers</param>
     public void GetLoadout(ref BaseType[] equippedBases, ref MixerType[] equippedMixers)
     {
         equippedBases = loadoutManager.GetEquippedBases();
@@ -51,7 +62,7 @@ public class GameManager : MonoBehaviour
     /// <summary>
     /// Load the given scene name and does the transition animation dependent on the scene.
     /// </summary>
-    /// <param name="sceneName"></param>
+    /// <param name="sceneName">Name of scene to transition to</param>
     public void LoadScene(string sceneName)
     {
         if (GetCurrentSceneName() == "Saloon")
@@ -60,14 +71,14 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-        StartCoroutine(TransitionAnim(sceneName));
+            StartCoroutine(TransitionAnim(sceneName));
         }
     }
+
     /// <summary>
     /// Animation dependent on the scene.
     /// </summary>
-    /// <param name="scene"></param>
-    /// <returns></returns>
+    /// <param name="scene">Name of the trigger animation</param>
     IEnumerator TransitionAnim(string scene)
     {
         FreezePlayer(true);
@@ -89,6 +100,11 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Called when a new scene is loaded. Triggers the animation.
+    /// </summary>
+    /// <param name="scene">The loaded scene</param>
+    /// <param name="mode">The scene load mode</param>
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         animator.SetTrigger("Scene Loaded");
@@ -98,7 +114,7 @@ public class GameManager : MonoBehaviour
     /// <summary>
     /// Load the given scene name
     /// </summary>
-    /// <param name="sceneName"></param>
+    /// <returns>The name of the current active scene</returns>
     public string GetCurrentSceneName()
     {
         return SceneManager.GetActiveScene().name;
@@ -107,7 +123,7 @@ public class GameManager : MonoBehaviour
     /// <summary>
     /// Toggles the player controller to freeze/unfreeze the player
     /// </summary>
-    /// <param name="freeze"></param>
+    /// <param name="freeze">Whether to freeze the player</param>
     public void FreezePlayer(bool freeze)
     {
         if (freeze)
@@ -125,6 +141,11 @@ public class GameManager : MonoBehaviour
         FreezePlayer(true);
         LoadScene(GetCurrentSceneName());
     }
+
+    /// <summary>
+    /// Called when a boss is defeated, loads the next scene.
+    /// </summary>
+    /// <param name="nextSceneName">Name of the next scene to load</param>
     public void BossDefeated(string nextSceneName)
     {
         LoadScene(nextSceneName);
