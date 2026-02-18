e12e87974a13ed42873154f225723425483e2251
jujud4 <jmegad4@gmail.com>
2026-02-18 16:35:27 -0500

    Granny Phase 2
    Began implementing the phase 2 form of the Granny Boss

---
+++ Assets/Scripts/Boss Scripts/GrannyPhase2.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GrannyPhase2 : Boss
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void SetPhase()
+    {
+        ///?
+    }
+} 
