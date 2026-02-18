06261e42bc875ec513032c7cfd8fa4323667d969
Angel <thesparkingofhope@gmail.com>
2026-02-18 16:40:03 -0500

    FireFlower

---
+++ Assets/Scripts/Boss Scripts/Fire_Flower.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Fire_Flower : MonoBehaviour, IDamageable
+{
+    [SerializeField] private float rotationRate;
+    [SerializeField] private int maxHealth;
+    [SerializeField] private GameObject fireLine;
+    private SpriteRenderer fireRenderer;
+    private float timer;
+    private float health;
+    private float totalRotation;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timer = 0;
+        health = maxHealth;
+        totalRotation = 0;
+        fireRenderer = fireLine.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        totalRotation += rotationRate * Time.deltaTime;
+        totalRotation = totalRotation % 360;
+        fireLine.transform.RotateAround(this.transform.position,Vector3.forward, rotationRate * Time.deltaTime);
+
+        if (totalRotation <90 || totalRotation > 270)
+        {
+            fireRenderer.sortingOrder = 1;
+        }
+        else
+        {
+            fireRenderer.sortingOrder = 2;
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health--;
+        if (health == 0)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
