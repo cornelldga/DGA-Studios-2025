212431ca179d96a209a63b7ba85930543199e2e0
Andrew Hu <94906754+Tofucs@users.noreply.github.com>
2026-03-11 18:32:10 -0400

    whip various sfx and mixer swap

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -151,7 +151,7 @@ public class Player : MonoBehaviour, IDamageable
         equippedImage.sprite = selectedBase.getSprite();
         backupImage.sprite = backupBase.getSprite();
         changeCooldown = changeCooldownTime;
-        AudioManager.Instance.PlaySFX(2); // i will remove magic numbers in the future :D
+        AudioManager.Instance.PlaySFX(SFXKey.BASESWAP, true); // i will remove magic numbers in the future :D
     }
 
     /// <summary>
@@ -168,6 +168,7 @@ public class Player : MonoBehaviour, IDamageable
         mixerEquippedImage.sprite = selectedMixer.getSprite();
         mixerBackupImage.sprite = backupMixer.getSprite();
         changeCooldown = changeCooldownTime;
+        AudioManager.Instance.PlaySFX(SFXKey.MIXERSWAP, true);
     }
 
     /// <summary>
@@ -263,10 +264,10 @@ public class Player : MonoBehaviour, IDamageable
         switch (equippedBases[baseIndex])
         {
             case BaseType.Gin:
-                AudioManager.Instance.PlaySFX(0, true);
+                AudioManager.Instance.PlaySFX(SFXKey.GIN, true);
                 break;
             case BaseType.Beer:
-                AudioManager.Instance.PlaySFX(1, true);
+                AudioManager.Instance.PlaySFX(SFXKey.BEER, true);
                 break;
         }
         
@@ -287,6 +288,7 @@ public class Player : MonoBehaviour, IDamageable
         Vector3 dir = transform.localScale.x < 0 ? - (world - whipPivot.position) : world - whipPivot.position;
         whipPivot.right = dir;
         whipAnimator.Play("Whip", 0, 0f);
+        AudioManager.Instance.PlaySFX(SFXKey.WHIPHIT, true);
         StartCoroutine(nameof(ToggleWhipUI));
     }
     /// <summary>
