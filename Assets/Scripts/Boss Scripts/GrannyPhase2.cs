b584708b4fdbb8c05bc800d39b09ffc129b0940c
jujud4 <jmegad4@gmail.com>
2026-03-23 17:17:44 -0400

    Granny Combo Attacks
    Added a script for granny combo attacks that involve single point projectiles

--- Assets/Scripts/Boss Scripts/GrannyPhase2.cs
+++ Assets/Scripts/Boss Scripts/GrannyPhase2.cs
@@ -169,8 +169,8 @@ public class GrannyPhase2 : Boss
             bullScript.ChargeSpecificDirection(Random.onUnitCircle);
             bullScript.setSummoned();
             
-            Trail bushTrail = bulls[i].AddComponent<Trail>();
-            bushTrail.SetTrailPrefab(bushPrefab);
+            BushTrail bushTrail = bulls[i].AddComponent<BushTrail>();
+            bushTrail.SetBushPrefab(bushPrefab);
         }
         yield return new WaitForSeconds(flamingBullsTime);
         for (int i = bulls.Count - 1; i >= 0; i--)
@@ -180,6 +180,10 @@ public class GrannyPhase2 : Boss
         }
     }
 
+    private void PointlAttacks()
+    {
+        
+    }
     private void TransitionToLazer()
     {
         currentState = State.Lazer;
