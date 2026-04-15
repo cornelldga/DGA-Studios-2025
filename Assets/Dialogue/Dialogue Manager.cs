51748a33e05beaaea3a8eede4cc6573320751b39
chvelez19 <chvelez19@gmail.com>
2026-04-15 18:05:54 -0400

    Reapply "Merge branch 'main' into charles/tutorial-continued"
    This reverts commit 731c82960831c9752cd9bc5db6229a5b432e8cc2.

--- Assets/Dialogue/Dialogue Manager.cs
+++ Assets/Dialogue/Dialogue Manager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using System.Diagnostics;
 
 /// <summary>
 /// The types of dialogue
@@ -128,6 +129,13 @@ public class DialogueManager : MonoBehaviour
         {
             gameObject.SetActive(true);
             nameText.text = file.name;
+            if (nameText.text=="Mirage & Ace")
+            {
+                dialogueText.color = Color.white;
+            } else
+            {
+                dialogueText.color = new Color(0.15f, 0.1f, 0.05f, 1.0f);
+            }
             ongoingDialogue = true;
             dialogueAnim.SetBool("isOpen", true);
             currentDialogueData = JsonUtility.FromJson<DialogueData>(file.text);
@@ -148,6 +156,18 @@ public class DialogueManager : MonoBehaviour
             }
             GameManager.Instance.FreezePlayer(true);
             DisplayNextLine();
+        } else
+        {
+            // Automatic player loadout
+            if (scene=="Loadout")
+            {
+                EndDialogue();
+                GameManager.Instance.ToggleLoadoutManager(true);
+            } else
+            {
+                // Automatic entry/exit for the saloon
+                GameManager.Instance.LoadScene(scene); 
+            }
         }
     }
 
@@ -282,14 +302,7 @@ public class DialogueManager : MonoBehaviour
     {
         yesButton.gameObject.SetActive(false);
         noButton.gameObject.SetActive(false);
-        if (nameText.text == "Loadout"){
-            EndDialogue();
-            GameManager.Instance.ToggleLoadoutManager(true);
-        }
-        else
-        {
-            GameManager.Instance.LoadScene(sceneName);
-        }
+         GameManager.Instance.LoadScene(sceneName);
     }
 
     /// <summary>
